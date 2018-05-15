762e9fa0911daf62a4db3a86a054576649bc7423
Henrique Nascimento <henriquenascandrade@gmail.com>
2018-05-14 23:30:38 -0300

    (*-*)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom'
-import './App.css';
-import Projects from './components/Projects';
-import Labs from './components/Labs';
-import About from './components/About';
-import NotFound from './components/NotFound';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import Projects from "./components/Projects";
+import Labs from "./components/Labs";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">            
-            <Link to='/'>About</Link>
-            <Link to='/projects'>Projects</Link>
-            <Link to='/labs'>Labs</Link>
+          <header className="App-header">
+            <Link to="/henriquenasc">About</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/labs">Labs</Link>
           </header>
-          <Route exact path='/' component={About} />
-          <Route exact path='/projects' component={Projects} />
-          <Route exact path='/labs' component={Labs} />
+          <Route exact path="/henriquenasc" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/labs" component={Labs} />
           {/* <Route exact path='*' component={NotFound} /> */}
         </div>
       </Router>
