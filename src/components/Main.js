0cc21eb33a04046eaebc4734b2f5e23919b8a447
Henrique Nascimento <henriquenascandrade@gmail.com>
2019-08-10 11:51:54 -0300

    new version - my personal website reactjs

---
+++ src/components/Main.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="section">
+      <div className="description">
+        <p className="text-header">
+          Hi! My name is <span className="text-name">Henrique Nascimento</span>.
+          I’m a 25 year-old web developer.
+        </p>
+
+        <p className="sub-text">
+          Computer enthusiast, passionate about new technologies and people. I
+          like communities like (open source, makers, developers and others).
+        </p>
+        <p className="sub-text">
+          Currently studying things like{" "}
+          <span className="span-js">javascript</span> and{" "}
+          <span className="span-php">PHP</span>. You can follow my projects in
+          my github.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <div>
+        <a>About</a>
+        <a>Projects</a>
+      </div>
+    </nav>
+  );
+}
