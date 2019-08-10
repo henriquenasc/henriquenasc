0076b8081af63ce4ad9b76f79b2be5d0f6b4b239
Henrique Nascimento <henriquenascandrade@gmail.com>
2019-08-10 12:01:54 -0300

    Updates

---
+++ precache-manifest.f6921a6ea85d1bd31a0018f9c4c62db4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8d57610876c9f18a8e0dbf60e2286e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b00789fabca7c8128b2",
+    "url": "/static/css/main.d1c1f4bf.chunk.css"
+  },
+  {
+    "revision": "8d0a2393cedba46a3612",
+    "url": "/static/js/2.856c8a76.chunk.js"
+  },
+  {
+    "revision": "9b00789fabca7c8128b2",
+    "url": "/static/js/main.6448edcf.chunk.js"
+  },
+  {
+    "revision": "981c8b764c9c10cfcc3a",
+    "url": "/static/js/runtime~main.e0f87755.js"
+  },
+  {
+    "revision": "3767138f84d91a4e2909a6860622515b",
+    "url": "/static/media/github.3767138f.svg"
+  },
+  {
+    "revision": "eab36d798336b2d0e9c6ffd639d129b2",
+    "url": "/static/media/gmail.eab36d79.svg"
+  },
+  {
+    "revision": "dd70291be8b8a8b4ce4f70ea364fac44",
+    "url": "/static/media/linkedin.dd70291b.svg"
+  },
+  {
+    "revision": "8db08d4b60ee76e1f8e69358d1af5e37",
+    "url": "/static/media/stackoverflow.8db08d4b.svg"
+  },
+  {
+    "revision": "f14bedbf1abb389347b15a28b43405a3",
+    "url": "/static/media/twitter.f14bedbf.svg"
+  }
+]);
