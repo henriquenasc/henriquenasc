3085785c7eb3399a7455c854d5bc1acb9ea51fea
Henrique Nascimento <henriquenascandrade@gmail.com>
2019-08-10 23:42:29 -0300

    add navigation without refresh - react-router-dom Link

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <nav>
-      <div>
-        <a>About</a>
-        <a>Projects</a>
-      </div>
+      <Link to="/">About</Link>
+      <Link to="/projects">Projects</Link>
     </nav>
   );
 }
